6d393659263352e664dbc7a538d04bc25407c43c
Shrunal Nimje <shrunalnimje@gmail.com>
2025-02-28 13:34:14 +0530

    Initial Commit

---
+++ src/main/java/my/mood/ExpenceTracker/Expence_Tracker/ExpenceTrackerApplication.java
@@ -0,0 +1,13 @@
+package my.mood.ExpenceTracker.Expence_Tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenceTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenceTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/my/mood/ExpenceTracker/Expence_Tracker/ExpenceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package my.mood.ExpenceTracker.Expence_Tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenceTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

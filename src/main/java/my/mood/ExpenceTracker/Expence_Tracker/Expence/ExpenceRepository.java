b01277645c7d0f21964bf37453387b66970434ff
Shrunal Nimje <shrunalnimje@gmail.com>
2025-03-01 13:03:15 +0530

    Create Expense repo interface

---
+++ src/main/java/my/mood/ExpenceTracker/Expence_Tracker/Expence/ExpenceRepository.java
@@ -0,0 +1,9 @@
+package my.mood.ExpenceTracker.Expence_Tracker.Expence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ExpenceRepository extends JpaRepository<Expence_Entity, Integer>{
+
+}

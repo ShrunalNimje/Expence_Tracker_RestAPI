fd31b98d7c116050e922c9fcd612ab26650ba8be
Shrunal Nimje <shrunalnimje@gmail.com>
2025-03-02 09:17:07 +0530

    added pagination & sorting

--- src/main/java/my/mood/ExpenceTracker/Expence_Tracker/Expence/ExpenceController.java
+++ src/main/java/my/mood/ExpenceTracker/Expence_Tracker/Expence/ExpenceController.java
@@ -4,12 +4,17 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
@@ -27,12 +32,20 @@ public class ExpenceController {
 //         this.expenceService = expenceService;
         this.expenceRepository = expenceRepository;
     }
-
+    
     @GetMapping("/expences")
-    public List<Expence_Entity> retrieveAllExpences() {
-        // return expenceService.retrieveAll();
-    	return expenceRepository.findAll();
+    public Page<Expence_Entity> retrieveAllExpences(
+        @RequestParam(defaultValue = "0") int page, 
+        @RequestParam(defaultValue = "5") int size,
+        @RequestParam(defaultValue = "id") String sortBy) {
+        
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sortBy));
+        return expenceRepository.findAll(pageable);
+        
+		// return expenceService.retrieveAll();
+		// return expenceRepository.findAll();
     }
+
 
     @GetMapping("/expences/{id}")
     public Optional<Expence_Entity> retrieveSpecificExpence(@PathVariable int id) {
@@ -80,6 +93,12 @@ public class ExpenceController {
     		throw new RuntimeException("Expence not found with ID = " + id); 
     	}
     	
+    }
+    
+    @GetMapping("/expences/category/{category}")
+    public List<Expence_Entity> retrieveByCategory(@PathVariable String category) {
+        // return expenceService.retrieveFromId(id);
+    	return expenceRepository.findByCategory(category);
     }
     
 }

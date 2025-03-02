d022956018d93587f9f16c349629d0c3b2ae1868
Shrunal Nimje <shrunalnimje@gmail.com>
2025-03-02 19:34:51 +0530

    updating basic authentication

--- src/main/java/my/mood/ExpenceTracker/Expence_Tracker/Security/SecurityConfig.java
+++ src/main/java/my/mood/ExpenceTracker/Expence_Tracker/Security/SecurityConfig.java
@@ -6,6 +6,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -24,9 +30,24 @@ public class SecurityConfig {
 		http.httpBasic(withDefaults());
 		
 		// 3) CSRF -> post, put
-		http.csrf().disable();
+		http.csrf(csrf-> csrf.disable());
 		
 		return http.build();
 	}
+	
+	@Bean
+	public UserDetailsService userDetailsService() {
+	    UserDetails user = User.withUsername("Shrunal")
+	            .password(passwordEncoder().encode("dummy")) // 🔥 Encode the password!
+	            .roles("USER")
+	            .build();
+
+	    return new InMemoryUserDetailsManager(user);
+	}
+	
+	@Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(); 
+    }
 	
 }

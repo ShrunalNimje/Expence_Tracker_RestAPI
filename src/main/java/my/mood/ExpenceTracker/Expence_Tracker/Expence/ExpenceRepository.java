21c05d8f80c707a0663dd24a0b8027875eac636c
Shrunal Nimje <shrunalnimje@gmail.com>
2025-03-02 09:17:33 +0530

    added custom method

--- src/main/java/my/mood/ExpenceTracker/Expence_Tracker/Expence/ExpenceRepository.java
+++ src/main/java/my/mood/ExpenceTracker/Expence_Tracker/Expence/ExpenceRepository.java
@@ -1,9 +1,13 @@
 package my.mood.ExpenceTracker.Expence_Tracker.Expence;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ExpenceRepository extends JpaRepository<Expence_Entity, Integer>{
+
+	List<Expence_Entity> findByCategory(String category);
 
 }
